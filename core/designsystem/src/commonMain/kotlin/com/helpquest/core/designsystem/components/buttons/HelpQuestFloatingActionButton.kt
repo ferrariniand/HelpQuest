85300c8d4de0c09fe4baaf92c866eec23edf870d
it_anfe0028 <andrea.ferrarini@partner.italdesign.it>
2025-09-25 08:53:42 +0200

    added Floating Action Button in design system

---
+++ core/designsystem/src/commonMain/kotlin/com/helpquest/core/designsystem/components/buttons/HelpQuestFloatingActionButton.kt
@@ -0,0 +1,62 @@
+package com.helpquest.core.designsystem.components.buttons
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.helpquest.core.designsystem.theme.HelpQuestTheme
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+
+@Composable
+fun HelpQuestFloatingActionButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    FloatingActionButton(
+        onClick = onClick,
+        modifier = modifier,
+        shape = RoundedCornerShape(8.dp),
+        containerColor = MaterialTheme.colorScheme.primary,
+        contentColor = MaterialTheme.colorScheme.onPrimary,
+        content = content
+    )
+}
+
+@Composable
+@Preview
+fun HelpQuestFloatingActionButtonLightPreview() {
+    HelpQuestTheme {
+        HelpQuestFloatingActionButton(
+            onClick = {}
+        ) {
+            Icon(
+                imageVector = Icons.Default.Add,
+                contentDescription = null
+            )
+        }
+    }
+}
+
+@Composable
+@Preview
+fun HelpQuestFloatingActionButtonDarkPreview() {
+    HelpQuestTheme (
+        darkTheme = true
+    ) {
+        HelpQuestFloatingActionButton(
+            onClick = {}
+        ) {
+            Icon(
+                imageVector = Icons.Default.Add,
+                contentDescription = null
+            )
+        }
+    }
+}
