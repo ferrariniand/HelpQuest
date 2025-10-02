7532b8869be065088922b382802c6e57addccd79
it_anfe0028 <andrea.ferrarini@partner.italdesign.it>
2025-10-02 12:51:00 +0200

    added BottomSheet in design system

---
+++ core/designsystem/src/commonMain/kotlin/com/helpquest/core/designsystem/components/dialogs/HelpQuestBottomSheet.kt
@@ -0,0 +1,37 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package com.helpquest.core.designsystem.components.dialogs
+
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HelpQuestBottomSheet(
+    onDismiss: () -> Unit,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    val sheetState = rememberModalBottomSheetState(
+        skipPartiallyExpanded = true  //TODO: maybe will be used in a different BottomSheet
+    )
+    LaunchedEffect(sheetState.isVisible) {
+        if (sheetState.isVisible) {
+            sheetState.expand()
+        }
+    }
+
+    ModalBottomSheet(
+        onDismissRequest = onDismiss,
+        dragHandle = null, //TODO: maybe will be used in a different BottomSheet
+        contentWindowInsets = { WindowInsets() },
+        modifier = modifier.statusBarsPadding()
+    ) {
+        content()
+    }
+}
