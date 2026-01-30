48c529bc91d88594fa70e9225a76be01ae4d9c2a
it_anfe0028 <andrea.ferrarini@partner.italdesign.it>
2026-01-30 16:03:03 +0100

    Added rememberImagePickerLauncher in core presentation

---
+++ core/presentation/src/desktopMain/kotlin/com/helpquest/core/presentation/mediapicker/rememberImagePickerLauncher.desktop.kt
@@ -0,0 +1,8 @@
+package com.helpquest.core.presentation.mediapicker
+
+import androidx.compose.runtime.Composable
+
+@Composable
+actual fun rememberImagePickerLauncher(onResult: (PickedImageData) -> Unit): ImagePickerLauncher {
+    TODO("Not yet implemented")
+}
